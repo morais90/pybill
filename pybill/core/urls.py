da6209249341d89a3a16ca60bb3dea79cc00957c
Willian Morais <williandmorais@gmail.com>
2019-07-31 10:03:10 -0300

    Fix scheme documentation

--- pybill/core/urls.py
+++ pybill/core/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 
 
-API_TITLE = 'pybill Calls Documentation'
+API_TITLE = 'Pybill Documentation'
 API_DESCRIPTION = """
-    pybill Calls is an application that helps in tracking the calls that your company makes.
+    Pybill is an application that helps in tracking the calls that your company makes.
     Through it, it's possible to monitor and audit in real time all the data necessary for good management.
     The relevant data reports allow you to take care of your business in a much simpler way.
 """
